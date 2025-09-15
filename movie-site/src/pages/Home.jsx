33c192a537e2f601d4ffbbadc3ee8bf85d914226
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-09-15 22:16:38 +1000

    feat: basic search field

--- movie-site/src/pages/Home.jsx
+++ movie-site/src/pages/Home.jsx
@@ -5,10 +5,24 @@ function Home () {
         {id: 1, title: "Piartes", release_date: "2024"},
         {id: 2, title: "John Wick", release_date: "2013"},
         {id: 3, title: "Intersetllar", release_date: "2014"},
-    ]
+    ];
+
+    const handleSearch = () => {
+
+    }
 
     return (
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search for movies..." 
+                    className="search-input"
+                />
+                <button type="submit" className="search-button">Search</button>
+                
+            </form>
+
             <div className="moives-grid">
                 {movies.map(movie => (
                     <MovieCard movie={movie} key={movie.id} />
