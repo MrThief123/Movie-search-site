e4a20c360644218968162b120f466518ab645f57
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-09-15 22:27:44 +1000

    fix: store query in state

--- movie-site/src/pages/Home.jsx
+++ movie-site/src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from "react"
 
 function Home () {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1, title: "Piartes", release_date: "2024"},
         {id: 2, title: "John Wick", release_date: "2013"},
         {id: 3, title: "Intersetllar", release_date: "2014"},
     ];
 
-    const handleSearch = () => {
-
-    }
+    const handleSearch = (e) => {
+        e.preventDefault()
+    };
 
     return (
         <div className="home">
@@ -18,6 +21,8 @@ function Home () {
                     type="text" 
                     placeholder="Search for movies..." 
                     className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
                 <button type="submit" className="search-button">Search</button>
                 
