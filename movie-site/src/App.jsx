2a03d836bba444c1ab2ef622759146b43097a69d
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-09-17 14:01:04 +1000

    feat: create NavBar

--- movie-site/src/App.jsx
+++ movie-site/src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Favorites from './pages/Favorites'
 import Home from './pages/home'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/favorites" element={<Favorites />}></Route>
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+        </Routes>
+      </main>
+    </div>
   )
 } 
 
