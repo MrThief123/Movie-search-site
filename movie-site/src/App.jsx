35342eed46d9428ecfa9f9d3bb3a0213639a1b48
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-09-17 13:56:20 +1000

    feat: simple page routing

--- movie-site/src/App.jsx
+++ movie-site/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Favorites from './pages/Favorites'
 import Home from './pages/home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <Home />
-    </div>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/favorites" element={<Favorites />}></Route>
+      </Routes>
+    </main>
   )
 } 
 
