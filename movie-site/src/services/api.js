e6a668a08579cb6ff6f73d425cbe877c088c01bf
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-09-20 18:46:40 +1000

    fix: api call

--- movie-site/src/services/api.js
+++ movie-site/src/services/api.js
@@ -1,11 +1,16 @@
 
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularMovies = async () => {
-    const reponse = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    const data = await reponse.json;
-    return data.results
+  try {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+  } catch (err) {
+    console.error(err);
+    return []; 
+  }
 };
 
 export const searchMovies = async (query) => {
